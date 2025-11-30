b31bbe6af855056c5e46b2deafa0fee05d16c682
Yilikal Tesfaye <yilikalt.kebede@gmail.com>
2025-11-30 14:48:36 +0300

    feat(login): enhance layout and styling of login page with improved design and structure

--- src/pages/public/Login.tsx
+++ src/pages/public/Login.tsx
@@ -2,23 +2,61 @@ import { Link } from "react-router";
 
 const Login = () => {
 	return (
-		<div className="flex flex-row justify-evenly">
-			<div>
-				<h2>Welcome back to Carent</h2>
-				<p>the best car rental paltform</p>
+		<div className="flex h-screen w-full">
+			<div className="relative w-1/2 h-full">
+				<img
+					src="/images/logup.jpg"
+					alt=""
+					className="absolute  w-full h-full object-cover "
+				/>
+				<div className="absolute inset-0 bg-black/50 flex flex-col justify-end p-10 text-white">
+					<h2 className="text-3xl font-semibold uppercase">
+						Welcome back to Carent
+					</h2>
+					<p className="text-lg ">The best car rental platform</p>
+				</div>
 			</div>
-			<div>
-				<h1 className="italic font-bold font-serif text-lg">Carent.</h1>
-				<h2>Log in to your account</h2>
-				<form action="" className="flex flex-col">
-					<label htmlFor="username">Username</label>
-					<input type="text" name="username" id="username" />
-					<label htmlFor="password">Password</label>
-					<input type="password" name="password" id="password" />
-					<button type="submit">Log In</button>
+			<div className="w-1/2 h-full flex flex-col justify-center px-20">
+				<Link to="/">
+					<h1 className="italic font-bold font-serif text-2xl mb-10">
+						Carent.
+					</h1>
+				</Link>
+
+				<h2 className="text-3xl font-bold mb-6">Log in to your account</h2>
+
+				<form className="flex flex-col gap-2 max-w-2xl">
+					<div className="flex flex-col">
+						<label htmlFor="username" className="font-semibold font-mono">
+							Username
+						</label>
+						<input
+							type="text"
+							name="username"
+							id="username"
+							className="border p-2"
+						/>
+					</div>
+					<div className="flex flex-col">
+						<label htmlFor="password" className="font-semibold font-mono">
+							Password
+						</label>
+						<input
+							type="password"
+							name="password"
+							id="password"
+							className="border p-2"
+						/>
+					</div>
+					<button type="submit" className="mt-4 bg-black text-white py-2 ">
+						Log In
+					</button>
 				</form>
-				<p>
-					Don't have an account? <Link to={"/register"}>Register Here</Link>
+				<p className="mt-4">
+					Don't have an account?{" "}
+					<Link to={"/register"} className="underline">
+						Register Here
+					</Link>
 				</p>
 			</div>
 		</div>
