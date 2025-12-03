489fb22cc439d427670dc83075bb24e3f6086fa0
Yilikal Tesfaye <yilikalt.kebede@gmail.com>
2025-12-03 23:32:42 +0300

    feat: add react-hook-form and zod dependencies to package.json and integrate React Query for data fetching and add QueryClient setup

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -3,9 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router";
 import router from "./routes";
+import { QueryClientProvider } from "@tanstack/react-query";
+import queryClient from "./api/clients";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<RouterProvider router={router} />
+		<QueryClientProvider client={queryClient}>
+			<RouterProvider router={router} />
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
 	</StrictMode>
 );
