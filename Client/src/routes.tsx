c4bacc429295b88a7f8436bd5dfdd2f47ff7df3f
Yilikal Tesfaye <yilikalt.kebede@gmail.com>
2025-12-03 14:30:10 +0300

    fix: update import paths to include .tsx extensions in routes and configuration files

--- Client/src/routes.tsx
+++ Client/src/routes.tsx
@@ -1,27 +1,27 @@
 // src/router.tsx
 import { createBrowserRouter } from "react-router";
-import App from "./App";
+import App from "./App.tsx";
 
 // Public
-import Home from "./pages/public/Home";
-import Login from "./pages/public/Login";
-import Register from "./pages/public/Register";
+import Home from "./pages/public/Home.tsx";
+import Login from "./pages/public/Login.tsx";
+import Register from "./pages/public/Register.tsx";
 
 // User
-import UserLayout from "./pages/user/UserLayout";
-import UserHome from "./pages/user/UserHome";
-import CarDetail from "./pages/user/CarDetail";
-import RentPage from "./pages/user/RentPage";
-import UserProfile from "./pages/user/UserProfile";
-import UserOrders from "./pages/user/UserOrders";
+import UserLayout from "./pages/user/UserLayout.tsx";
+import UserHome from "./pages/user/UserHome.tsx";
+import CarDetail from "./pages/user/CarDetail.tsx";
+import RentPage from "./pages/user/RentPage.tsx";
+import UserProfile from "./pages/user/UserProfile.tsx";
+import UserOrders from "./pages/user/UserOrders.tsx";
 
 // // Admin
-import AdminLayout from "./pages/admin/AdminLayout";
+import AdminLayout from "./pages/admin/AdminLayout.tsx";
 import AdminDashboard from "./pages/admin/AdminDashboard.tsx";
-import AdminCars from "./pages/admin/AdminCars";
-import AdminRentals from "./pages/admin/AdminRentals";
-import AdminUsers from "./pages/admin/AdminUsers";
-import AdminCategories from "./pages/admin/AdminCategories";
+import AdminCars from "./pages/admin/AdminCars.tsx";
+import AdminRentals from "./pages/admin/AdminRentals.tsx";
+import AdminUsers from "./pages/admin/AdminUsers.tsx";
+import AdminCategories from "./pages/admin/AdminCategories.tsx";
 
 // // Auth
 // import RequireAuth from "./auth/RequireAuth";
