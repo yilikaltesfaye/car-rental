2a3d37d5a31895fd02506463b1a8922ee0dd43ee
Yilikal Tesfaye <yilikalt.kebede@gmail.com>
2025-12-04 22:28:26 +0300

    feat: add description field to Category model and update serializer to include it

--- server/catalog/models/category.py
+++ server/catalog/models/category.py
@@ -4,6 +4,7 @@ from django.db import models
 class Category(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255, unique=True)
+    description = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
-        return self.name
+        return f"{self.name} - {self.description}"
--- server/catalog/serializers/category.py
+++ server/catalog/serializers/category.py
@@ -4,4 +4,4 @@ from catalog.models.category import Category
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ["id", "name"]
+        fields = ["id", "name", "description"]
